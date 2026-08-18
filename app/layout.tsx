cc72a81eb82f1ee682cbef5f95776cb9f1074a7f
fardeenpathan <md.fardeen5236@gmail.com>
2026-08-18 11:57:37 +0530

    update video

--- app/layout.tsx
+++ app/layout.tsx
@@ -47,6 +47,13 @@ export const metadata: Metadata = {
   },
 };
 
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+  viewportFit: "cover",
+};
 
 
 export default function RootLayout({
